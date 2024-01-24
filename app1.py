e17fdb3bfd178eec98c6e35443b4608aa2bf3a12
sdhackney <157060982+sdhackney@users.noreply.github.com>
2024-01-24 07:58:12 -0800

    added mean app1.py

--- app1.py
+++ app1.py
@@ -1,7 +1,26 @@
 import streamlit as st
 
 st.header('Tossing a Coin')
+chart = st.line_chart([0.5])
 
+def toss_coin(n):
+
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no +=1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+
+    return mean
+    
 number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
 start_button = st.button('Run')
 
