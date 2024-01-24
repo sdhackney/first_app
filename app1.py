a8f773fd759d7dce24cbd470d51cd2f9216ac26d
sdhackney <157060982+sdhackney@users.noreply.github.com>
2024-01-24 08:02:45 -0800

    app will run now app1.py

--- app1.py
+++ app1.py
@@ -26,5 +26,6 @@ start_button = st.button('Run')
 
 if start_button:
     st.write(f'Running the experient of {number_of_trials} trials.')
+    mean = toss_coin(number_of_trials)
 
 st.write('It is not a functional application yet. Under construction.')
